0b17363470783339a355de99734f379d709d353f
Aroktyoe <Aroktyoe@gmail.com>
2025-06-04 16:05:09 +0200

    test-logo + test html + test css

--- frontend/app.js
+++ frontend/app.js
@@ -1,11 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'frontend/views'));
+
+// Static files
+app.use('/css', express.static(path.join(__dirname, 'frontend/css')));
+app.use('/img', express.static(path.join(__dirname, 'frontend/img')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
