92063c8b0eb4e9a59c9d9992367dd3464fba64e5
Aroktyoe <Aroktyoe@gmail.com>
2025-06-04 18:18:04 +0200

    backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../frontend/views'));
+
+// Static files
+app.use('/css', express.static(path.join(__dirname, '../frontend/css')));
+app.use('/img', express.static(path.join(__dirname, '../frontend/img')));
+app.use('/src', express.static(path.join(__dirname, '../frontend/src')));
+
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
