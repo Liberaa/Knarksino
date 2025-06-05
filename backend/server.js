90d12c49e91b04e569ee51a0297c26c0805ccd30
Aroktyoe <Aroktyoe@gmail.com>
2025-06-05 18:04:56 +0200

    login

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const session = require('express-session');
+
+app.use(session({
+  secret: 'knarksinoSecret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+  }
+}));
+
+// Parse incoming request bodies
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // View engine
 app.set('view engine', 'ejs');
@@ -11,9 +25,17 @@ app.use('/css', express.static(path.join(__dirname, '../frontend/css')));
 app.use('/img', express.static(path.join(__dirname, '../frontend/img')));
 app.use('/src', express.static(path.join(__dirname, '../frontend/src')));
 
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  next();
+});
+
 // Use routes
 const mainRoutes = require('./routes/mainRoutes');
 app.use('/', mainRoutes);
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/', authRoutes);
 
 // Server
 const PORT = process.env.PORT || 3000;
